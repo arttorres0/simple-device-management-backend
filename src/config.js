58feec5de51c2930e22378536366f64de256a6e7
Arthur Torres <arttorres2004@hotmail.com>
2021-03-19 23:25:33 -0400

    Add database connection using sequelize

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ dotenv.config();
 
 module.exports = {
   port: process.env.SERVER_PORT || 3000,
+  dbUri: process.env.DB_URI,
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
-
+const db = require("./database/db");
 const { port } = require("./config");
 
 app.listen(port, () => {
   console.log("Server running on port: " + port);
 });
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Connected to database successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 module.exports = app;
