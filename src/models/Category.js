88edeffa95b6c279cbcfe46c3f2afbf60bc5acd7
Arthur Torres <arttorres2004@hotmail.com>
2021-03-19 23:54:56 -0400

    Add category model

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const db = require("../database/db");
+
+const Category = db.define("category", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(128),
+    allowNull: false,
+  },
+});
+
+module.exports = Category;
