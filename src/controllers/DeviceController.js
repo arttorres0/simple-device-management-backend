785741e6b9e6558365333efce2d1f87511f59b28
Arthur Torres <arttorres2004@hotmail.com>
2021-03-20 23:38:16 -0400

    Add find all devices route

--- src/controllers/DeviceController.js
+++ src/controllers/DeviceController.js
@@ -22,3 +22,47 @@ exports.create = async (req, res) => {
     });
   }
 };
+
+exports.findAll = async (req, res) => {
+  var filterColor = req.query.color || "";
+  var filterCategoryId = req.query.categoryId || "";
+  var filterPartNumber = req.query.partNumber || "";
+  var page = req.query.page || 1;
+  var pageSize = 10;
+
+  try {
+    const devices = await Device.findAndCountAll({
+      include: ["category"],
+      limit: pageSize,
+      offset: pageSize * page - pageSize,
+      where: {
+        [Sequelize.Op.and]: [
+          Sequelize.where(Sequelize.fn("lower", Sequelize.col("color")), {
+            [Sequelize.Op.like]: "%" + filterColor.toLowerCase() + "%",
+          }),
+          {
+            categoryId: {
+              [Sequelize.Op.like]: "%" + filterCategoryId + "%",
+            },
+          },
+          {
+            partNumber: {
+              [Sequelize.Op.like]: "%" + filterPartNumber + "%",
+            },
+          },
+        ],
+      },
+    });
+
+    return res.send({
+      devices: devices.rows,
+      page,
+      pageSize: pageSize,
+      numberOfResults: devices.count,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      message: err.message || "Error getting list of devices.",
+    });
+  }
+};
--- src/routes/devices.js
+++ src/routes/devices.js
@@ -2,4 +2,5 @@ module.exports = (routes) => {
   const devices = require("../controllers/DeviceController");
 
   routes.post("/devices", devices.create);
+  routes.get("/devices", devices.findAll);
 };
